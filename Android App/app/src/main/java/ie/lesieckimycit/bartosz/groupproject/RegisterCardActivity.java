fbde5fe8d333fe4b7e4e87b3dcdba33e74ae0a17
Bartosz Lesiecki <43273176+blesiecki@users.noreply.github.com>
2018-12-30 23:06:40 +0000

    Working on registering credit card details

--- Android App/app/src/main/java/ie/lesieckimycit/bartosz/groupproject/RegisterCardActivity.java
+++ Android App/app/src/main/java/ie/lesieckimycit/bartosz/groupproject/RegisterCardActivity.java
@@ -1,15 +1,35 @@
 package ie.lesieckimycit.bartosz.groupproject;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.anjlab.android.iab.v3.BillingProcessor;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterCardActivity extends AppCompatActivity{
+    private EditText mCardNumber, mCCV;
+    private Button mRegister;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     Spinner mMonth = findViewById(R.id.month);
+    Spinner mYear = findViewById(R.id.year);
+
 
 
     @Override
@@ -17,6 +37,42 @@ public class RegisterCardActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_card);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(RegisterCardActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        mCardNumber = findViewById(R.id.cardNumber);
+        mCCV = findViewById(R.id.CCV);
+        String[] months = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+        String[] years = new String[]{"2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"};
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, months);
+        mMonth.setAdapter(monthAdapter);
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, years);
+        mYear.setAdapter(yearAdapter);
+
+        mRegister = findViewById(R.id.register);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mCardNumber.getText().toString();
+                final String password = mCCV.getText().toString();
+
+
+            }
+        });
+        
     }
 
 }
